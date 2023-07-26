b5e88efb9be3c658d3846790b028cb2478a1a37b
Teddy10000 <Tedakrong12@gmail.com>
2023-07-26 23:10:45 +0000

    done

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-console */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto();
+  const user = createUser();
+
+  return Promise.all([photo, user]).then((resp) => {
+    console.log(`${resp[0].body} ${resp[1].firstName} ${resp[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
+}
